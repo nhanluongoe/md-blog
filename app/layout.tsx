646b10564c998586458437b7275d1b1ef40b1163
nhanluongoe <nhanlt18@gmail.com>
2025-09-07 22:36:02 +0700

    chore: add favicon and move static to public

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,29 @@ export default function RootLayout({
         GeistMono.variable,
       )}
     >
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/static/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/static/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/static/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/static/favicons/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/static/favicons/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
